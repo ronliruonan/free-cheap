b09bc614cb4d91f29aca2bb9540f5114e8b43df9
Ronan <liruonan@hotmail.com>
2020-07-11 10:00:21 +0800

    update: 从头来

--- week14/COMPONENT/main.js
+++ week14/COMPONENT/main.js
@@ -1,133 +1,120 @@
 /**
- * author @winter
+ * author @ronan
  */
-function createElement(Cls, attributes, ...children){
-    
-    let o;
 
-    if(typeof Cls === "string") {
-        o = new Wrapper(Cls);
-    } else {
-        o = new Cls({
-            timer: {}
-        });
-    }
+function createElement (Cls, attributes, ...children) {
+
+  let o;
+
+  if (typeof Cls === 'string') {
+    o = new Wrapper(Cls);
+  } else {
+    o = new Cls({
+      timer: {}
+    });
+  }
 
 
 
-    for(let name in attributes) {
-        o.setAttribute(name, attributes[name]);
-    }
 
-    //console.log(children);
-    console.log(o);
-    for(let child of children) {
-        if(typeof child === "string")
-            child = new Text(child);
+  for (let name in attributes) {
+    o.setAttribute(name, attributes[name]);
+  }
 
-        o.appendChild(child);
-    }
+  for (let child of children) {
+    if (typeof child === 'string')
+      child = new Text(child);
 
-    return o;
+    o.appendChild(child);
+  }
+
+  return o;
 }
 
 class Text {
-    constructor(text){
-        this.children = [];
-        this.root = document.createTextNode(text);
-    }
-    mountTo(parent){
-        parent.appendChild(this.root);
-    }
+  constructor(text) {
+    this.children = [];
+    this.root = document.createTextNode(text)
+  }
+
+  mountTo (parent) {
+    parent.appendChild(this.root);
+  }
 }
 
-class Wrapper{
-    constructor(type){
-        this.children = [];
-        this.root = document.createElement(type);
+class Wrapper {
+  constructor(type) {
+    this.children = [];
+    this.root = document.createElement(type)
+  }
+
+  setAttribute (name, value) { // attribute
+    this.root.setAttribute(name, value);
+  }
+
+  appendChild (child) { // children
+    this.children.push(child);
+  }
+
+  mountTo (parent) {
+    parent.appendChild(this.root);
+
+    for (let child of this.children) {
+      child.mountTo(this.root)
     }
-
-    setAttribute(name, value) { //attribute
-        this.root.setAttribute(name, value);
-    }
-
-    appendChild(child){
-        this.children.push(child);
-
-    }
-
-    mountTo(parent){
-        parent.appendChild(this.root);
-
-        for(let child of this.children){
-            child.mountTo(this.root);
-        }
-    }
-
+  }
 }
 
 class MyComponent {
-    constructor(config){
-        this.children = [];
+  constructor(config) {
+    this.children = [];
+  }
+
+  setAttribute (name, value) { // attribute
+    console.warn('这里好像不会运行')
+    this.root.setAttribute(name, value);
+  }
+
+  appendChild (child) { // children
+    this.children.push(child);
+  }
+
+  render () {
+
+    return <artical>
+      <header>I'm a header</header>
+      {this.slot}
+      <footer>I'm a footer</footer>
+    </artical>
+  }
+
+  mountTo (parent) {
+    this.slot = <div></div>
+
+    for (let child of this.children) {
+      debugger;
+      this.slot.appendChild(child);
     }
 
-    setAttribute(name, value) { //attribute
-        this.root.setAttribute(name, value);
-    }
-
-    appendChild(child){
-        this.children.push(child);
-    }
-
-    render(){
-        
-        return <article>
-            <header>I'm a header</header>
-            {this.slot}
-            <footer>I'm a footer</footer>
-        </article>
-    }
-
-    mountTo(parent){
-        this.slot = <div></div>
-        for(let child of this.children){
-            debugger;
-            this.slot.appendChild(child)
-        }
-        this.render().mountTo(parent)
-
-    }
-
-
+    this.render().mountTo(parent);
+  }
 }
 
-
-/*let component = <div id="a" cls="b" style="width:100px;height:100px;background-color:lightgreen">
-        <div></div>
-        <p></p>
-        <div></div>
-        <div></div>
-    </div>*/
+// let component = <div id='a' class="b">
+//   <div></div>
+//   <div></div>
+//   <div></div>
+// </div>
 
 let component = <MyComponent>
-    <div>text text text</div>
+  <p>text</p>
 </MyComponent>
-    
 
 component.mountTo(document.body);
-/*
-var component = createElement(
-    Parent, 
-    {
-        id: "a",
-        "class": "b"
-    }, 
-    createElement(Child, null), 
-    createElement(Child, null), 
-    createElement(Child, null)
-);
-*/
+
+// // <Parent id='a' class="b"> attributes
+// // component.id = 'iddd' property
+// component.id = 'iddd';
 
 console.log(component);
-
-//componet.setAttribute("id", "a");
+// component.setAttribute('id', 'a');
